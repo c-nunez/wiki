46a796b837b8fc9854f2d8da27767a3b2e8df7ec
c-nunez <nunez_cr@outlook.com>
2022-01-27 10:54:09 -0500

    wiki search func completed

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="newpage"),
-    path("random", views.random, name="random")
+    path("search", views.search, name="search"),
+    path("wiki/<str:entry>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,53 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from markdown2 import Markdown
+from django import forms
 from . import util
+import secrets
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, entry): 
+    markdowner = Markdown()
+    entryPage = util.get_entry(entry)
+    if entryPage is None:
+        return render(request, "encyclopedia/noentry.html", {
+            "entryTitle": entry
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdowner.convert(entryPage),
+            "entryTitle": entry
+        })
+        
+def search(request):
+    value = request.GET.get('q')
+    if(util.get_entry(value) is not None):
+        return HttpResponseRedirect(reverse("entry", kwargs= {
+            'entry': value
+        }))
+    else:
+        subStringEntries = []
+        for entry in util.list_entries():
+            if value.upper() in entry.upper():
+                subStringEntries.append(entry)
+        return render(request, "encyclopedia/index.html", {
+            "entries": subStringEntries,
+            "search": True,
+            "value": value
+        })
 
 def new(request):
     return render(request, "encyclopedia/newpage.html")
 
 def random(request):
-    return render(request, "encyclopedia/random.html")
+    entries = util.list_entries()
+    randomEntry = secrets.choice(entries)
+    return HttpResponseRedirect(reverse("entry", kwargs={
+        'entry': randomEntry
+        }))
